7d79442173966d1d3b44ac2422ebab4231249a25
Filipe Chaves <flschaves@gmail.com>
2020-08-22 19:59:00 -0300

    Cria componente TopBar

--- src/components/TopBar/index.js
+++ src/components/TopBar/index.js
@@ -1,7 +1,28 @@
 import React from "react";
 
+import PersonIcon from "@material-ui/icons/Person";
+import LocationIcon from "@material-ui/icons/LocationOn";
+import { WhatsAppIcon } from "../icons";
+
+import styles from "./index.module.scss";
+
 const TopBar = () => {
-	return <div>TopBar</div>
+	return (
+		<div className={styles.topbar}>
+			<div className={styles.topbar__item}>
+				<WhatsAppIcon width="14" style={{ marginLeft: "5px", marginRight: "5px" }} />
+				(47) 9 9999-9999
+			</div>
+			<div className={styles.topbar__item}>
+				<PersonIcon />
+				Arethusa
+			</div>
+			<div className={styles.topbar__item}>
+				<LocationIcon />
+				Bom Retiro - Joinville, SC
+			</div>
+		</div>
+	);
 };
 
 export default TopBar;
