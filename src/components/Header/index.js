fc738222e8b17f0f03c0c6e13ac26c0793aa450f
Filipe Chaves <flschaves@gmail.com>
2020-08-22 20:55:51 -0300

    Cria componente para Logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import TopBar from "../TopBar";
-
-import logo from "./logo.png";
+import Logo from "../Logo";
 
 const Header = () => {
 	return (
 		<header>
 			<div className="container">
 				<TopBar />
-				<img src={logo} alt="Logo" />
+				<Logo />
 			</div>
 		</header>
 	);
---
+++ src/components/Logo/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+import logo from "../../assets/img/logo.png";
+
+const Logo = () => {
+	return (
+		<div className={styles.logo}>
+			<img src={logo} alt="Logo" />
+		</div>
+	);
+};
+
+export default Logo;
