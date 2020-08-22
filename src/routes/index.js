142450ee1649be163899fc0918f0fd11fa196314
Filipe Chaves <flschaves@gmail.com>
2020-08-22 20:42:31 -0300

    Reescreve rotas utilizando React.lazy e altera ordem das rotas da mais específica pra mais geral

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Cart from "../pages/Cart";
-import Checkout from "../pages/Checkout";
-import NotFound from "../pages/NotFound";
+const Cart = React.lazy(() => import("../pages/Cart"));
+const Checkout = React.lazy(() => import("../pages/Checkout"));
+const NotFound = React.lazy(() => import("../pages/NotFound"));
 
 const Routes = () => {
 	return (
-		<Router>
-			<Switch>
-				<Route exact path="/" component={Cart} />
-				<Route path="/checkout" component={Checkout} />
-				<Route path="*" component={NotFound} />
-			</Switch>
-		</Router>
+		<React.Suspense fallback={<div>Carregando...</div>}>
+			<Router>
+				<Switch>
+					<Route exact path="/checkout" component={Checkout} />
+					<Route exact path="/" component={Cart} />
+					<Route component={NotFound} />
+				</Switch>
+			</Router>
+		</React.Suspense>
 	);
 };
 
