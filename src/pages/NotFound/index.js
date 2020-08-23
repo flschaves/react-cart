f5c5e1da416491ee4b2bc11d2b375bd2c067733d
Filipe Chaves <flschaves@gmail.com>
2020-08-23 01:02:45 -0300

    Adiciona componente Layout na página NotFound

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Layout from "../../components/Layout";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return <Layout title="Ops, conteúdo não encontrado!">Parece que o conteúdo que você está buscando não existe!</Layout>;
 };
 
 export default NotFound;
