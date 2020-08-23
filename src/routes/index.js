77661fe44abc8e730eff416c42dbbb2d999fe8ed
Filipe Chaves <flschaves@gmail.com>
2020-08-22 22:57:17 -0300

    Adiciona Loading do Material UI no React.Suspense

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 const Cart = React.lazy(() => import("../pages/Cart"));
 const Checkout = React.lazy(() => import("../pages/Checkout"));
 const NotFound = React.lazy(() => import("../pages/NotFound"));
 
 const Routes = () => {
   return (
-    <React.Suspense fallback={<div>Carregando...</div>}>
+    <React.Suspense fallback={<CircularProgress />}>
       <Router>
         <Switch>
           <Route exact path="/checkout" component={Checkout} />
