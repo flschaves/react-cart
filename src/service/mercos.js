a5b7c145c19cf2e98753d3b85c65391873973019
Filipe Chaves <flschaves@gmail.com>
2020-08-24 13:16:55 -0300

    Altera proxy para CORS

--- src/service/mercos.js
+++ src/service/mercos.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const corsURL = "https://cors-anywhere.herokuapp.com/";
+// const corsURL = "https://cors-anywhere.herokuapp.com/";
+const corsURL = "https://thingproxy.freeboard.io/fetch/";
 
 export default axios.create({
   baseURL: `${corsURL}https://sandbox.mercos.com/api-teste-front/`,
