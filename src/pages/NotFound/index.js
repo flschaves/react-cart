3d0243e4759bbd424114c2a0ff4da9a9572f46e3
Filipe Chaves <flschaves@gmail.com>
2020-08-23 22:55:56 -0300

    Centraliza texto de conteúdo não encontrado

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import Layout from "../../components/Layout";
 
 const NotFound = () => {
-  return <Layout title="Ops, conteúdo não encontrado!">Parece que o conteúdo que você está buscando não existe!</Layout>;
+  return (
+    <Layout title="Ops, conteúdo não encontrado!">
+      <p style={{ textAlign: "center" }}>
+        Parece que o conteúdo que você está buscando não existe!
+      </p>
+    </Layout>
+  );
 };
 
 export default NotFound;
