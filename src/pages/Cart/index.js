7852e16ddd821d09456629541282ea5a7365ab0e
Filipe Chaves <flschaves@gmail.com>
2020-08-24 00:33:13 -0300

    Cria estilos para página Cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import CartList from "../../components/CartList";
+import CartResume from "../../components/CartResume";
+
+import styles from "./index.module.scss";
 
 const Cart = () => {
-  return <Layout title="Carrinho">Carrinho</Layout>;
+  return (
+    <Layout title="Carrinho">
+      <div className={styles["page-cart"]}>
+        <div className={styles["page-cart--list"]}>
+          <CartList />
+        </div>
+        <div className={styles["page-cart--resume"]}>
+          <CartResume />
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Cart;
