6c5fced3ccdc6a7ab102cd7930e72f2065615e53
Filipe Chaves <flschaves@gmail.com>
2020-08-24 00:31:51 -0300

    Cria componente CartResume

---
+++ src/components/CartResume/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+const CartResume = () => {
+  return <div className={styles["cart-resume"]}>Resumo</div>;
+};
+
+export default CartResume;
