875cbaeb250cf8ddfb7bbdf746627662172bb8b0
Filipe Chaves <flschaves@gmail.com>
2020-08-24 21:27:26 -0300

    Exibe valor do carrinho no MiniCart

--- src/components/Minicart/index.js
+++ src/components/Minicart/index.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+
+import { CartContext } from "../../context/CartContext";
+
+import formatPrice from "../../helpers/formatPrice";
 
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
 import styles from "./index.module.scss";
 
 const Minicart = () => {
+  const {
+    state: { total },
+  } = useContext(CartContext);
+
   return (
     <Link to="/" className={styles.minicart}>
       <ShoppingCartIcon />
-      <span className={styles.minicart__price}>R$ 62,50</span>
+      <span className={styles.minicart__price}>{formatPrice(total)}</span>
     </Link>
   );
 };
