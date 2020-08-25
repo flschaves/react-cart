43564f5b88674da7da2440f35073a0c2b56c5f81
Filipe Chaves <flschaves@gmail.com>
2020-08-24 23:45:31 -0300

    Adiciona propriedade onClick no componente Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,9 +2,9 @@ import React from "react";
 
 import styles from "./index.module.scss";
 
-const Button = ({ style, children }) => {
+const Button = ({ style, onClick, children }) => {
   return (
-    <button style={style} className={styles.button}>
+    <button style={style} onClick={onClick} className={styles.button}>
       {children}
     </button>
   );
